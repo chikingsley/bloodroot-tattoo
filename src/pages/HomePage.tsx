73d1c1bb98363c6f8543cdaea5b45b82e67b4d4d
Chi <cheez2012@gmail.com>
2025-05-05 05:15:15 +0700

    +hero

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import BloodrootLogo from '../components/logo/bloodroot-logo';
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <h1>Home Page Placeholder</h1>
-      {/* Content for the home page will go here */}
+    <div 
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: 'calc(100vh - 160px)', // Adjust height based on potential header/footer
+        padding: '2rem'
+      }}
+    >
+      <BloodrootLogo 
+        fillColor="white" 
+        style={{
+          width: '90%', // Responsive width
+          maxWidth: '600px', // Max size
+          height: 'auto'
+        }}
+      />
     </div>
   );
 };
